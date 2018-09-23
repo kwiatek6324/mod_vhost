8d1d179408e09d610e268931125628dfd3ee557b
Kwiatek <andrzej.kwiatkowski@gmail.com>
2018-09-23 11:43:20 +0200

    Removig BerkleyDB Cache, replacing with Apache OPCache

--- mod_vhost.c
+++ mod_vhost.c
@@ -13,6 +13,13 @@
 #include "apr_reslist.h"
 #include "apr_file_io.h"
 
+#include "ap_config.h"
+#include "ap_provider.h"
+#include "ap_socache.h"
+#include "util_mutex.h"
+
+
+
 
 
 #ifdef HAVE_PGSQL
@@ -37,13 +44,12 @@
 
 
 #include <time.h>
-#include <db.h>
 
 #include <string.h>
 #include <strings.h>
 
 
-
+#define MAX_LEN 1024
 #define PGSQL_PORT	5432
 
 
@@ -89,7 +95,38 @@ typedef struct {
 #endif
         char    *debug;
 	apr_array_header_t	*aliases;
+
+	int timeout;
+	apr_array_header_t *providers;
+	const char *context;
+
 } mod_vhost_config;
+
+
+static apr_global_mutex_t *mod_vhost_mutex = NULL;
+static ap_socache_provider_t *socache_provider = NULL;
+static ap_socache_instance_t *socache_instance = NULL;
+static const char *const mod_vhost_id = "mod_vhost-socache";
+static int configured;
+
+static apr_status_t remove_lock(void *data)
+{
+    if (mod_vhost_mutex) {
+        apr_global_mutex_destroy(mod_vhost_mutex);
+        mod_vhost_mutex = NULL;
+    }
+    return APR_SUCCESS;
+}
+
+static apr_status_t destroy_cache(void *data)
+{
+    if (socache_instance) {
+        socache_provider->destroy(socache_instance, (server_rec*)data);
+        socache_instance = NULL;
+    }
+    return APR_SUCCESS;
+}
+
 
 
 typedef struct {
@@ -127,7 +164,7 @@ return con;
 };
 
 
-PGresult *pgsql_tuples(PGconn *con,char sql[1024])
+PGresult *pgsql_tuples(PGconn *con,char sql[MAX_LEN])
 {
 PGresult        *res;
 
@@ -190,7 +227,7 @@ return mysql;
 };
 
 
-MYSQL_RES *mysql_tuples(MYSQL *con,char sql[1024])
+MYSQL_RES *mysql_tuples(MYSQL *con,char sql[MAX_LEN])
 {
 int     i;
 
@@ -229,7 +266,7 @@ if (rc!=0) {
 
 
 
-int sqlite_tuples(server_rec *s,sqlite3 *sqlite,char sql[1024],char ***wynik, int *cnt)
+int sqlite_tuples(server_rec *s,sqlite3 *sqlite,char sql[MAX_LEN],char ***wynik, int *cnt)
 {
 int     rc;
 
@@ -259,111 +296,92 @@ static char *get_db_cache(server_rec *s,request_rec *r,char *prefix,char *hostna
 {
 mod_vhost_config   *vc;
 
-DB                      *dbp;
-DBT                     key, data;
-
 int                     ret;
 char                    *dr=NULL;
-char			tmp[1024];
+char			key[MAX_LEN];
+
+unsigned char val[MAX_LEN];
+unsigned int vallen = MAX_LEN - 1;
 
 
 vc=ap_get_module_config(r->server->module_config, &mod_vhost_module);
 
-if (hostname==NULL || dbfile==NULL)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: no hostname/dbfile received by get_db_cache");
+if (hostname==NULL || dbfile==NULL) {
+        ap_log_rerror(APLOG_MARK,APLOG_WARNING,0,r,APLOGNO(00000) "[mod_vhost.c]: no hostname/dbfile received by get_db_cache");
         return NULL;
         };
 
-if ((ret = db_create(&dbp, NULL, 0)) != 0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: db_create: %s", db_strerror(ret)); return NULL;
-        }
+apr_status_t rv;
+apr_snprintf(key,MAX_LEN,"%s:%s:%s",dbfile,prefix,hostname);
 
-if ((ret = dbp->open(dbp, NULL,dbfile, NULL, DB_BTREE, DB_CREATE, 0664)) != 0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: db_open: %s", dbfile); return NULL;
-        }
 
-memset(&key, 0, sizeof(key));
-memset(&data, 0, sizeof(data));
+rv = socache_provider->retrieve(socache_instance, r->server, (unsigned char*)key, strlen(key), val, &vallen, r->pool);
 
-apr_snprintf(tmp,1024,"%s:%s",prefix,hostname);
+if (APR_STATUS_IS_NOTFOUND(rv)) { 
+    if (debug>0) { ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, APLOGNO(00001) "[mod_vhost.c]: Cache entry not found for [%s]",key); }
+    return NULL; 
+  } else 
+if (rv == APR_SUCCESS) { 
+    val[vallen] = 0; 
+   if (debug>0)  ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, APLOGNO(00002) "[mod_vhost.c]: Cache entry found for [%s]=>[%s]", key,val);
+  } else {
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(0003) "[mod_vhost.c]: Error accessing cache [%s]",key);
+    return NULL;
+}
 
-key.data = (char *)(tmp);
-key.size = strlen(tmp);
 
-if (debug>0) { ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_db_cache: [%s] hostname: %s[%d]",dbfile,key.data,key.size); };
-
-if ((ret = dbp->get(dbp, NULL, &key, &data, 0)) == 0)
-        {
-        dr = apr_palloc(r->pool, data.size + 1);
-        strncpy(dr,data.data,data.size);
-        dr[data.size]='\0';
-        if (debug>0) { ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_db_cache [%s]: got: %s[%d]",tmp,dr,data.size); };
-        };
-
-if ((ret=dbp->close(dbp, 0))!=0)
-        {
-        dbp->err(dbp, ret, "DB->put");
-        };
-
-return dr;
+return apr_pstrdup(r->pool,val);
 
 }
 
 
 static char *set_db_cache(server_rec *s,request_rec *r,char *prefix,char *hostname,char *value,char *dbfile,int debug)
 {
-DB                      *dbp;
-DBT                     key, data;
 
 int                     ret;
 char                    *dr=NULL;
 
-char			tmp[1024];
+char			key[MAX_LEN];
+mod_vhost_config	*vc;
 
-if (hostname==NULL || value==NULL || dbfile==NULL)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: set_db_cache: no hostname/docroot/dbfile received by set_db_cache ");
-        return NULL;
-        };
+apr_status_t rv;
+apr_time_t expiry;
 
-if ((ret = db_create(&dbp, NULL, 0)) != 0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s, "db_create: %s", db_strerror(ret)); exit (1);
-        }
+vc=ap_get_module_config(r->server->module_config, &mod_vhost_module);
 
-if ((ret = dbp->open(dbp, NULL,dbfile, NULL, DB_BTREE, DB_CREATE, 0664)) != 0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: set_db_cache: db_open: %s", dbfile); return NULL;
-        return NULL;
-        }
+if (!configured ) { return NULL; }
 
-memset(&key, 0, sizeof(key));
-memset(&data, 0, sizeof(data));
+if (hostname==NULL || value==NULL || dbfile==NULL) {
+  ap_log_error(APLOG_MARK,APLOG_WARNING,0,s, APLOGNO(00010) "[mod_vhost.c]: set_db_cache: no hostname/docroot/dbfile received by set_db_cache ");
+  return NULL;
+  };
 
-apr_snprintf(tmp,1024,"%s:%s",prefix,hostname);
 
-key.data = (char *)(tmp);
-key.size = strlen(tmp);
+apr_snprintf(key,MAX_LEN,"%s:%s:%s",dbfile,prefix,hostname);
 
-data.data=value;
-data.size=strlen(value);
+rv = apr_global_mutex_trylock(mod_vhost_mutex);
+if (APR_STATUS_IS_EBUSY(rv)) {
+  ap_log_rerror(APLOG_MARK, APLOG_WARNING, rv, r, APLOGNO(00011) "[mod_vhost.c]: set_db_cache: Cache mutex busy for [%s]", key);
+  return NULL;
+  } else 
+if (rv != APR_SUCCESS) {
+  ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(00012) "[mod_vhost.c]: set_db_cache: Failed to cache for [%s]", key);
+  return NULL;
+}
 
-if (debug>0) { ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: set_db_cache [%s][%d]: [%s][%d]",key.data,key.size,data.data,data.size); }
+expiry = apr_time_now() + apr_time_from_sec(vc->timeout);
 
-if ((ret = dbp->put(dbp, NULL, &key, &data, 0)) != 0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: set_db_cache: error setting documentroot");
-        };
+rv = socache_provider->store(socache_instance, r->server, (unsigned char*)key, strlen(key), expiry, (unsigned char*)value, strlen(value), r->pool);
+if (rv == APR_SUCCESS) {
+  ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r, APLOGNO(00013) "[mod_vhost.c]: set_db_cache: Cached for [%s]->[%s]", key, value);
+  } else {
+  ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(00014) "[mod_vhost.c]: set_db_cache: Failed to cache for [%s]->[%s]", key, value);
+}
 
-if ((ret=dbp->close(dbp, 0))!=0)
-        {
-        ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: set_db_cache: error closing db");
-        };
+rv = apr_global_mutex_unlock(mod_vhost_mutex);
+if (rv != APR_SUCCESS) { ap_log_rerror(APLOG_MARK, APLOG_ERR, rv, r, APLOGNO(00015) "[mod_vhost.c]: set_db_cache: Failed to release mutex!"); }
 
-return dr;
+return NULL;
 
 };
 
@@ -374,7 +392,7 @@ static char *get_pgsql_webinfo(server_rec *s,request_rec *r,char *hostname,int d
 mod_vhost_config   	*vc;
 
 char                    *dr=NULL;
-char                    filter[1024];
+char                    filter[MAX_LEN];
 char			*val;
 
 PGconn                  *conn;
@@ -396,7 +414,9 @@ if (debug>0) {
 	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: pass: %s",vc->pgsql_pass);
 	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: db: %s",vc->pgsql_db);
 	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: select: %s",vc->pgsql_select);
-	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: database: %s",vc->poscache);
+	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: Positive Ident: %s",vc->poscache);
+	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: Negative Ident: %s",vc->negcache);
+	ap_log_error(APLOG_MARK, APLOG_WARNING, 0,s, "[mod_vhost.c]: CONF: SOCache Timeout : %d",vc->timeout);
 	}
 
 
@@ -417,7 +437,7 @@ if (vc->pgsql_host==NULL ||
                 ap_log_error(APLOG_MARK,APLOG_CRIT,0,s,"[mod_vhost.c]: get_pgsql_dr: Cant bind to pgsql server");
         } else {
                 if (debug>0) { ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_pgsql_dr: Connection established."); }
-                apr_snprintf(filter,1024,vc->pgsql_select,r->hostname);
+                apr_snprintf(filter,MAX_LEN,vc->pgsql_select,r->hostname);
 
                 if (debug>0) { ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_pgsql_dr: Filter: %s",filter); }
                 res=pgsql_tuples(conn,filter);
@@ -465,7 +485,7 @@ static char *get_ldap_webinfo(server_rec *s,request_rec *r,char *hostname)
 mod_vhost_config       *vc;
 
 char                    *dr=NULL;
-char                    filter[1024];
+char                    filter[MAX_LEN];
 char                    **val;
 
 LDAP                    *conn;
@@ -509,7 +529,7 @@ if (vc->ldap_host==NULL ||
                 ap_log_error(APLOG_MARK,APLOG_CRIT,0,s,"mod_vhost.c: get_ldap_dr: Cant bind to ldap server");
         } else {
                 ap_log_error(APLOG_MARK,APLOG_NOTICE,0,s,"mod_vhost.c: get_ldap_dr: Connection established.");
-                apr_snprintf(filter,1024,vc->ldap_filter,r->hostname);
+                apr_snprintf(filter,MAX_LEN,vc->ldap_filter,r->hostname);
 
                 ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"mod_vhost.c: get_ldap_dr: Filter: %s",filter);
                 res=ldap_search_s(conn,vc->ldap_basedn,scope,filter,NULL,0,&msg);
@@ -554,7 +574,7 @@ static char *get_mysql_webinfo(server_rec *s,request_rec *r,char *hostname)
 mod_vhost_config        *vc;
 
 char                    *dr=NULL;
-char                    filter[1024];
+char                    filter[MAX_LEN];
 char                    *val;
 int                     n;
 
@@ -597,7 +617,7 @@ if (vc->mysql_host==NULL ||
                 ap_log_error(APLOG_MARK,APLOG_CRIT,0,s,"[mod_vhost.c]: get_mysql_webinfo: cant connect to SQL server");
         } else {
                 ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_mysql_webinfo: connection established.");
-                apr_snprintf(filter,1024,vc->mysql_select,r->hostname);
+                apr_snprintf(filter,MAX_LEN,vc->mysql_select,r->hostname);
 
                 ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_mysql_webinfo: select: %s",filter);
                 res=mysql_tuples(conn,filter);
@@ -643,7 +663,7 @@ static char *get_sqlite_webinfo(server_rec *s,request_rec *r,char *hostname)
 mod_vhost_config        *vc;
 
 char                    *dr=NULL;
-char                    filter[1024];
+char                    filter[MAX_LEN];
 char                    *val;
 int                     n;
 
@@ -676,7 +696,7 @@ if (vc->sqlite_db==NULL || vc->sqlite_select==NULL) {
                 ap_log_error(APLOG_MARK,APLOG_CRIT,0,s,"[mod_vhost.c]: get_sqlite_webinfo: cant connect to SQLte server");
         } else {
                 ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_sqlite_webinfo: connection established.");
-                apr_snprintf(filter,1024,vc->sqlite_select,r->hostname);
+                apr_snprintf(filter,MAX_LEN,vc->sqlite_select,r->hostname);
 
                 ap_log_error(APLOG_MARK,APLOG_WARNING,0,s,"[mod_vhost.c]: get_sqlite_webinfo: select: %s",filter);
                 rc=sqlite_tuples(s,conn,filter,(char ***)&wynik,&cnt);
@@ -765,7 +785,7 @@ char *documentroot;
 char *php_version;
 char *uri;
 char		*dr=NULL;
-char		filter[1024];
+char		filter[MAX_LEN];
 int		debug=0;
 
 
@@ -886,6 +906,70 @@ return DECLINED;
 };
 
 
+static void mod_vhost_child_init(apr_pool_t *p, server_rec *s)
+{
+    const char *lock;
+    apr_status_t rv;
+    if (!configured) { return;}
+    lock = apr_global_mutex_lockfile(mod_vhost_mutex);
+    rv = apr_global_mutex_child_init(&mod_vhost_mutex, lock, p);
+    if (rv != APR_SUCCESS) { ap_log_error(APLOG_MARK, APLOG_CRIT, rv, s, APLOGNO(00020) "[mod_vhost.c]: error initialising mutex"); }
+}
+
+static int mod_vhost_precfg(apr_pool_t *pconf, apr_pool_t *plog, apr_pool_t *ptmp)
+{
+    apr_status_t rv = ap_mutex_register(pconf, mod_vhost_id, NULL, APR_LOCK_DEFAULT, 0);
+    if (rv != APR_SUCCESS) {
+        ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(00030) "[mod_vhost.c]: error registering mutex [%s]", mod_vhost_id);
+        return 500; 
+      }
+    socache_provider = ap_lookup_provider(AP_SOCACHE_PROVIDER_GROUP, AP_SOCACHE_DEFAULT_PROVIDER, AP_SOCACHE_PROVIDER_VERSION);
+    configured = 0;
+    return OK;
+}
+
+static int mod_vhost_post_config(apr_pool_t *pconf, apr_pool_t *plog,
+                                   apr_pool_t *ptmp, server_rec *s)
+{
+    apr_status_t rv;
+    static struct ap_socache_hints mod_vhost_hints = {64, 32, 60000000};
+    const char *errmsg;
+
+    if (!configured) { return OK;    }
+
+    if (socache_provider == NULL) {
+        ap_log_perror(APLOG_MARK, APLOG_CRIT, 0, plog, APLOGNO(00040) "[mod_vhost.c]:Please select socache provider with ModVhostSOCache");
+        return 500; 
+    }
+
+    if (socache_instance == NULL) {
+        errmsg = socache_provider->create(&socache_instance, NULL, ptmp, pconf);
+        if (errmsg) {
+            ap_log_perror(APLOG_MARK, APLOG_CRIT, 0, plog, APLOGNO(00041) "[mod_vhost.c]: Failed to create mod_socache_shmcb socache instance: %s", errmsg);
+            return 500;
+        }
+    }
+
+    rv = ap_global_mutex_create(&mod_vhost_mutex, NULL, mod_vhost_id, NULL, s, pconf, 0);
+    if (rv != APR_SUCCESS) {
+        ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(00042) "[mod_vhost.c]: Failed to create mutex[%s]", mod_vhost_id);
+        return 500; 
+    }
+    apr_pool_cleanup_register(pconf, NULL, remove_lock, apr_pool_cleanup_null);
+
+    rv = socache_provider->init(socache_instance, mod_vhost_id, &mod_vhost_hints, s, pconf);
+    if (rv != APR_SUCCESS) {
+        ap_log_perror(APLOG_MARK, APLOG_CRIT, rv, plog, APLOGNO(00043) "[mod_vhost.c]: Failed to initialise cache [%s]", mod_vhost_id);
+        return 500; 
+    }
+    apr_pool_cleanup_register(pconf, (void*)s, destroy_cache, apr_pool_cleanup_null);
+    return OK;
+}
+
+
+
+
+
 /***** config *****/
 
 static void *mod_vhost_create_cfg(apr_pool_t *p, server_rec * s)
@@ -929,6 +1013,9 @@ vc->mysql_select="select documentroot,uid,gid from www where domainname=%s";
 vc->sqlite_db="/tmp/baza.db";
 vc->sqlite_select="select documentroot,uid,gid from www where domainname=%s";
 #endif
+
+vc->timeout=0;
+vc->context="modvhost";
 
 vc->debug=NULL;
 
@@ -1120,6 +1207,49 @@ mod_vhost_config *conf =
     return NULL;
 }
 
+static const char *mod_vhost_set_socache(cmd_parms *cmd, void *dummy, char *arg)
+  {
+ //   const char *errmsg = ap_check_cmd_context(cmd, GLOBAL_ONLY);
+    const char *sep, *name;
+    const char *errmsg;
+
+  //  if (errmsg) return errmsg;
+
+    /* Argument is of form 'name:args' or just 'name'. */
+    sep = ap_strchr_c(arg, ':');
+    if (sep) {
+        name = apr_pstrmemdup(cmd->pool, arg, sep - arg);
+        sep++;
+    }
+    else {
+        name = arg;
+    }
+
+    socache_provider = ap_lookup_provider(AP_SOCACHE_PROVIDER_GROUP, name, AP_SOCACHE_PROVIDER_VERSION);
+    if (socache_provider == NULL) {
+        errmsg = apr_psprintf(cmd->pool, "[mod_vhost.c]:Unknown socache provider '%s' ",arg);
+      } else {
+        errmsg = socache_provider->create(&socache_instance, sep, cmd->temp_pool, cmd->pool);
+    }
+
+    if (errmsg) {
+        errmsg = apr_psprintf(cmd->pool, "[mod_vhost.c]: ModVhostSOCache: %s", errmsg);
+      }
+    configured =1;
+    return errmsg;
+}
+
+static const char *mod_vhost_set_socache_timeout(cmd_parms *cmd, void *dummy, char *arg)
+  {
+  mod_vhost_config *conf = (mod_vhost_config *)ap_get_module_config(cmd->server->module_config, &mod_vhost_module);
+
+  conf->timeout=atoi(arg);
+  return NULL;
+}
+
+
+
+
 static const char *mod_vhost_set_alias(cmd_parms *cmd, void *dummy, char *fake, char *real)
 {
 mod_vhost_config *conf =
@@ -1154,6 +1284,9 @@ mod_vhost_config *conf =
 
 static void mod_vhost_register_hooks(apr_pool_t *p) {
 //	static const char * const vhSucc[]={ "mod_alias.c", NULL };
+		ap_hook_pre_config(mod_vhost_precfg, NULL, NULL, APR_HOOK_MIDDLE);
+		ap_hook_post_config(mod_vhost_post_config, NULL, NULL, APR_HOOK_MIDDLE);
+		ap_hook_child_init(mod_vhost_child_init, NULL,NULL,APR_HOOK_MIDDLE);
         ap_hook_translate_name(mod_vhost_trans_uri, NULL, NULL, APR_HOOK_FIRST);
 };
 
@@ -1163,6 +1296,8 @@ static const command_rec mod_vhost_cmds[] = {
 	AP_INIT_TAKE1("ModVhostSuExecEnable",(void *)mod_vhost_set_suexec_enable, NULL, RSRC_CONF, "Set on or off to disable mod_vhost suexec support"),
 	AP_INIT_TAKE2("ModVhostServer",(void *)mod_vhost_set_server, NULL, RSRC_CONF, "Set Server used for connection"),
 	AP_INIT_TAKE1("ModVhostPHPDisableSubStr",(void *)mod_vhost_set_phpdisstr, NULL, RSRC_CONF, "Set substring in docroot where disable PHP"),
+    AP_INIT_TAKE1("ModVhostSOCache",(void *)mod_vhost_set_socache , NULL, RSRC_CONF,"Socache provider"),
+    AP_INIT_TAKE1("ModVhostSOCacheTimeout",(void *)mod_vhost_set_socache_timeout , NULL, RSRC_CONF,"Socache Timeout in s"),
 #ifdef HAVE_SQL
 	AP_INIT_TAKE1("ModVhostUser",(void *)mod_vhost_set_user, NULL, RSRC_CONF, "Set User used for connection"),
 	AP_INIT_TAKE1("ModVhostPass",(void *)mod_vhost_set_pass, NULL, RSRC_CONF, "Set Pass used for connection"),
